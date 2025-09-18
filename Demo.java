844521c4691566267d94376fc57aa6af9f8f007d
MANOJ-SINGARAVELAN <manojsingaravelan@gmail.com>
2025-09-18 17:38:45 +0530

    ok

---
+++ Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo    
+{
+    public static void main(String a[])
+    {
+        AdvHuman obj=new AdvHuman();
+        int x=obj.add(1,2);
+        int y=obj.sub(5,6);
+        int u=obj.multi(6,8);
+        int j=obj.div(12,6);
+        System.out.println(x+":"+y+":"+u+":"+j);
+     
+    }
+
+
+}
